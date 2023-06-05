474fc5a3f851cbba1c6edebf7c200a23022b3c8a
kratik <kratik38@gmail.com>
2023-06-05 18:48:37 +0530

    new

---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login() {
+  const handleClick = () => {
+    const clientId = "0f37250dc33d4bee9f87d0c51dd4bcc6";
+    const redirectUrl = "http://localhost:3000";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-playback-position",
+      "user-top-read",
+      "user-read-recently-played",
+    ];
+
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
+  };
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt="spotify"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    background: black;
+    color: #49f585;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
