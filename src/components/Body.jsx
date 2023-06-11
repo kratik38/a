8d2add36e33bbcf3fac9236fb365db8079ece169
kratik <kratik38@gmail.com>
2023-06-12 01:35:49 +0530

    body completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ import { useStateProvider } from "../utils/StateProvider";
 import axios from "axios";
 import { reducerCases } from "../utils/Constants";
 
-export default function Body() {
+export default function Body({ headerBackground }) {
   const [{ token, selectedPlaylistId, selectedPlaylist }, dispatch] =
     useStateProvider();
   useEffect(() => {
@@ -44,8 +44,14 @@ export default function Body() {
     getInitialPlaylist();
   }, [token, dispatch, selectedPlaylistId]);
 
+  const msToMinutesAndSeconds = (ms) => {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000).toFixed(0);
+
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
   return (
-    <Container>
+    <Container headerBackground={headerBackground}>
       {selectedPlaylist && (
         <>
           <div className="playlist">
@@ -74,46 +80,46 @@ export default function Body() {
                   <AiFillClockCircle />
                 </span>
               </div>
-              <div className="tracks">
-                {selectedPlaylist.tracks.map(
-                  (
-                    {
-                      id,
-                      name,
-                      artists,
-                      image,
-                      duration,
-                      album,
-                      context_uri,
-                      track_number,
-                    },
-                    index
-                  ) => {
-                    return (
-                      <div className="row" key={id}>
-                        <div className="col">
-                          <span>{index + 1}</span>
+            </div>
+            <div className="tracks">
+              {selectedPlaylist.tracks.map(
+                (
+                  {
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },
+                  index
+                ) => {
+                  return (
+                    <div className="row" key={id}>
+                      <div className="col">
+                        <span>{index + 1}</span>
+                      </div>
+                      <div className="col detail">
+                        <div className="image">
+                          <img src={image} alt="track" />
                         </div>
-                        <div className="col detail">
-                          <div className="image">
-                            <img src={image} alt="track" />
-                          </div>
-                          <div className="info">
-                            <span className="name">{name}</span>
-                            <span>{artists}</span>
-                          </div>
-                          <div className="col">
-                            <span>{album}</span>
-                          </div>
-                          <div className="col">
-                            <span>{duration}</span>
-                          </div>
+                        <div className="info">
+                          <span className="name">{name}</span>
+                          <span>{artists}</span>
                         </div>
                       </div>
-                    );
-                  }
-                )}
-              </div>
+                      <div className="col">
+                        <span>{album}</span>
+                      </div>
+                      <div className="col">
+                        <span>{msToMinutesAndSeconds(duration)}</span>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
             </div>
           </div>
         </>
@@ -122,4 +128,71 @@ export default function Body() {
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  .playlist {
+    margin: 0 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    .image {
+      img {
+        height: 15rem;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+      }
+    }
+    .details {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      color: #e0dede;
+      .title {
+        color: white;
+        font-size: 4rem;
+      }
+    }
+  }
+  .list {
+    .header__row {
+      display: grid;
+      grid-template-columns: 0.3fr 3fr 2fr 0.1fr;
+      color: #dddcdc;
+      margin: 1rem 0 0 0;
+      position: sticky;
+      top: 15vh;
+      padding: 1rem 3rem;
+      transition: 0.3s ease-in-out;
+      background-color: ${({ headerBackground }) =>
+        headerBackground ? "rgba(0,0,0,0.7)" : "none"};
+    }
+    .tracks {
+      margin: 0 2rem;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 5rem;
+      .row {
+        padding: 0.5rem 1rem;
+        display: grid;
+        grid-template-columns: 0.3fr 3.1fr 1.9fr 0.1fr;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+        .col {
+          display: flex;
+          align-items: center;
+          color: #dddcdc;
+          img {
+            height: 40px;
+          }
+        }
+        .detail {
+          display: flex;
+          gap: 1rem;
+          .info {
+            display: flex;
+            flex-direction: column;
+          }
+        }
+      }
+    }
+  }
+`;
